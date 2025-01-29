f42a3c5ca3a77eac66eaef1d905dab0abd62a175
Asadullah404 <151784066+Asadullah404@users.noreply.github.com>
2025-01-29 14:58:34 +0500

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -43,7 +43,7 @@ const AdminDashboard = () => {
               to="/GenAllPdf"
               className="block px-4 py-2 rounded-lg text-center bg-blue-600 text-white hover:bg-blue-700 transition mt-4"
             >
-              Asadullah
+              Generate PDF
             </Link>
           </CardContent>
         </Card>
